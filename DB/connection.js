bdaa90facf9d88142fd21c7e3cbaca174a9bb057
Aditya Jain <jaditya8109@gmail.com>
2020-08-23 13:49:17 +0530

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-const URI = ('mongodb+srv://dbUser:dbUser@cluster0.ilktx.mongodb.net/<dbname>?retryWrites=true&w=majority');
+//const URI = ('mongodb+srv://dbUser:dbUser@cluster0.ilktx.mongodb.net/<dbname>?retryWrites=true&w=majority');
+
+const db = process.env.MONGODB_URL
 
 const connectDB = async() =>{
-    await mongoose.connect(URI,
+    await mongoose.connect(db,
         { useNewUrlParser: true, useUnifiedTopology: true});
     console.log("Atlas connected");
+} catch(err) {
+    console.error(err.message);
+    process.exit(1);
 }
 
 module.exports = connectDB;
